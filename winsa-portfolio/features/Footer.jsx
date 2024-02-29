b5beeb6ccbdb56e340138b539e426f42888e28ae
WinsaDaniel <winsadaniel7@gmail.com>
2024-03-01 00:36:51 +0300

    winsa portifolio first commit

---
+++ winsa-portfolio/features/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <div className="bg-green-500">footer</div>;
+}
+
+export default Footer;
---
+++ winsa-portfolio/features/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>love</div>;
+}
+
+export default Header;
